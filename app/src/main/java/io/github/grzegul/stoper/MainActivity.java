bc2a59aa318b412e58ec1834a65b5d30be29ef4c
grzegul <grzegul@gmail.com>
2016-08-24 13:32:17 +0200

    usuniety przycisk OK; dodane wprowadzanie czasu z zaznaczeniem

--- app/src/main/java/io/github/grzegul/stoper/MainActivity.java
+++ app/src/main/java/io/github/grzegul/stoper/MainActivity.java
@@ -2,12 +2,16 @@ package io.github.grzegul.stoper;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,8 +28,9 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnOK, btnStart, btnStop;
+    private Button btnStart, btnStop;
     private TextView textViewTime;
+    private EditText editTextTime;
     private int godziny = 60*60*1000;
     private int minuty = 60*1000;
     private int sekundy = 1000;
@@ -56,7 +61,27 @@ public class MainActivity extends AppCompatActivity {
     public void setInterwal(int interwal) {
         this.interwal = interwal;
     }
+    public void dataRead(EditText editTextTime){
+        String s =  editTextTime.getText().toString();
+        if((s.contains(":")) && ((s.split(":")).length>3)){
+            setSekundy(0);
+            setMinuty(0);
+            setGodziny(0);
+        } else if((s.contains(":")) && ((s.split(":")).length>2)){
+            setSekundy(Integer.valueOf(s.split(":")[2]));
+            setMinuty(Integer.valueOf(s.split(":")[1]));
+            setGodziny(Integer.valueOf(s.split(":")[0]));
+        }else if(s.contains(":")){
+            setSekundy(Integer.valueOf(s.split(":")[1]));
+            setMinuty(Integer.valueOf(s.split(":")[0]));
+            setGodziny(0);
 
+        }else{
+            setSekundy(0);
+            setMinuty(Integer.valueOf(s));
+            setGodziny(0);
+        }
+    }   //czytanie okna tekstowego
     
 
     @Override
@@ -64,39 +89,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnOK = (Button) findViewById(R.id.btnOK);
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
         textViewTime = (TextView) findViewById(R.id.textViewTime);
-        textViewTime.setText("hh:mm:ss");
+        editTextTime = (EditText) findViewById(R.id.editTextTime);
 
-        btnOK.setOnClickListener(new View.OnClickListener() {
+        editTextTime.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                EditText editTextTime = (EditText) findViewById(R.id.editTextTime);
-                String s =  editTextTime.getText().toString();
-                if((s.contains(":")) && ((s.split(":")).length>3)){
-                    setSekundy(0);
-                    setMinuty(0);
-                    setGodziny(0);
-                } else if((s.contains(":")) && ((s.split(":")).length>2)){
-                    setSekundy(Integer.valueOf(s.split(":")[2]));
-                    setMinuty(Integer.valueOf(s.split(":")[1]));
-                    setGodziny(Integer.valueOf(s.split(":")[0]));
-                }else if(s.contains(":")){
-                    setSekundy(Integer.valueOf(s.split(":")[1]));
-                    setMinuty(Integer.valueOf(s.split(":")[0]));
-                    setGodziny(0);
-
-                }else{
-                    setSekundy(0);
-                    setMinuty(Integer.valueOf(s));
-                    setGodziny(0);
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    dataRead(editTextTime); //moja metoda
+                    textViewTime.setText(getGodziny()+":"+getMinuty()+":"+getSekundy());
+                    handled = false;
                 }
-                textViewTime.setText(getGodziny()+":"+getMinuty()+":"+getSekundy());
-
+                return handled;
             }
         });
+        //TEN LISTENER ZDAJE SIĘ ANULOWAĆ PRZYCISK STOP
+        /*editTextTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (v == editTextTime) {
+                    if (!hasFocus) {
+                        // Close keyboard
+                        ((InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE))
+                                .toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+                    }
+                }
+            }
+        });*/
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,7 +138,6 @@ public class MainActivity extends AppCompatActivity {
     public class CounterClass extends CountDownTimer{
         public CounterClass(long millisInFuture, long countDownInterval){
                 super(millisInFuture, countDownInterval);
-
         }
 
         @Override
