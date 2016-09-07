dea89824364e2527a26510719dbd2b423b3e22f7
grzegul <grzegul@gmail.com>
2016-09-07 11:16:58 +0200

    kombinowanie z wersja komplilacji i minimalnej wersji

--- app/src/main/java/io/github/grzegul/stoper/MainActivity.java
+++ app/src/main/java/io/github/grzegul/stoper/MainActivity.java
@@ -16,9 +16,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
